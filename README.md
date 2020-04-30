5e9eccc921a229b96d4f349c996327ddd08f2691
AnnaGrace <anna.grace.conover@gmail.com>
2020-04-29 21:41:36 -0700

    Finished readMe and refactored

--- README.md
+++ README.md
@@ -8,3 +8,31 @@ The html must take the users time and date from the computer.
 If the time has passed, the background is gray, if the time is now, the background is red, if the time is later it is green. So the background must be responsive to the time.
 
 If you put an event down, if you click the save button it needs to save it to local storage. If you refresh and put a new event down, should it replace or add to the event? I think add, I think you should backspace out any event you don't want.
+
+Real Code:
+
+In HTMl:
+
+I added the form elements and the form inputs. I also added multiple ids and classes for css and javascript.
+
+In JS:
+
+Used selectors added to the activities columns to add numerical values. Later used this against the real local time variable to determine background color.
+
+Made value that took the real time and date, and then used moment formatting to create variable result with desired format.
+
+Created variable hourString to hold the number of the hour in military time in a string. Then created variable hourNumber with parse to make the string a usable number.
+
+Displayed the current date in the jumbotron
+
+Created function timeColor, which holds the value number that I created of each of the rows in the activity column against the hour in military time. I made the values correspond to the time in military time. For example, if it is 1:00 formNumber is 13. Added pre-defined class of background colors depending if the number was less than, more than, or equal to the real time.
+
+Ran that function for each of the sections.
+
+Ran function renderLastRegistered, which gets from the local storage the value put in previously, and puts them in the form.
+
+Created a click event for each save button that first checks to see if the value is empty. If it is, nothing happens. If it has a value, it puts that value into local storage.
+
+In CSS:
+
+I created two classes in css that stylized the forms.
--- workday.js
+++ workday.js
@@ -1,8 +1,7 @@
+
+
 var time9 = $("#time9");
 time9.value = 9;
-// var Form9 = $("#form9").value;
-// console.log(Form9)
-
 
 var time10 = $("#time10");
 time10.value = 10;
@@ -30,7 +29,6 @@ time5.value = 17;
 
 var d = new Date();
 var format = "LL";
-console.log(d);
 nowDate = d.toLocaleDateString()
 var result = moment(nowDate).format(format);
 
@@ -39,17 +37,16 @@ hourNumber = parseFloat(hourString);
 
 $("#currentDay").text(result); 
 
-//idGrab is time variable
-function timeColor(idGrab) {
+function timeColor(formNumber) {
   
-    if (idGrab.value > hourNumber) {
-        idGrab.addClass("class future");
+    if (formNumber.value > hourNumber) {
+        formNumber.addClass("class future");
     }
-    else if (idGrab.value === hourNumber) {
-        idGrab.addClass("class present");
+    else if (formNumber.value === hourNumber) {
+        formNumber.addClass("class present");
     }
-    else if (idGrab.value < hourNumber) {
-        idGrab.addClass("class past");
+    else if (formNumber.value < hourNumber) {
+        formNumber.addClass("class past");
     }
 }
 
@@ -85,11 +82,6 @@ function renderLastRegistered() {
     $("#form3").attr("placeholder", Form3);
     $("#form4").attr("placeholder", Form4);
     $("#form5").attr("placeholder", Form5);
-    
-    
-    console.log(Form9);
-  
-  
   }
 
 $(".btn9").on("click", function(event) {
@@ -97,7 +89,6 @@ $(".btn9").on("click", function(event) {
 
     var Form9 = document.querySelector("#form9").value;
     if (Form9 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form9", Form9);
@@ -110,7 +101,6 @@ $(".btn9").on("click", function(event) {
 
     var Form9 = document.querySelector("#form9").value;
     if (Form9 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form9", Form9);
@@ -123,7 +113,6 @@ $(".btn10").on("click", function(event) {
 
     var Form10 = document.querySelector("#form10").value;
     if (Form10 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form10", Form10);
@@ -136,7 +125,6 @@ $(".btn11").on("click", function(event) {
 
     var Form11 = document.querySelector("#form11").value;
     if (Form11 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form11", Form11);
@@ -149,7 +137,6 @@ $(".btn12").on("click", function(event) {
 
     var Form12 = document.querySelector("#form12").value;
     if (Form12 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form12", Form12);
@@ -162,7 +149,6 @@ $(".btn1").on("click", function(event) {
 
     var Form1 = document.querySelector("#form1").value;
     if (Form1 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form1", Form1);
@@ -175,7 +161,6 @@ $(".btn2").on("click", function(event) {
 
     var Form2 = document.querySelector("#form2").value;
     if (Form2 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form2", Form2);
@@ -188,7 +173,6 @@ $(".btn3").on("click", function(event) {
 
     var Form3 = document.querySelector("#form3").value;
     if (Form3 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form3", Form3);
@@ -201,7 +185,6 @@ $(".btn4").on("click", function(event) {
 
     var Form4 = document.querySelector("#form4").value;
     if (Form4 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form4", Form4);
@@ -214,84 +197,9 @@ $(".btn5").on("click", function(event) {
 
     var Form5= document.querySelector("#form5").value;
     if (Form5 === "") {
-        console.log("no text");
     }
     else {
     localStorage.setItem("Form5", Form5);
     }
 
 });
-
-
-// let myVar = [
-//     {
-//         time: "0900",
-//         event: "event09",
-//     },
-//     {
-//         time: "1000",
-//         event: "event10",
-//     },
-//     {
-//         time: "1100",
-//         event: "event11",
-//     },
-//     {
-//         time: "1200",
-//         event: "event12",
-//     },
-//     {
-//         time: "1300",
-//         event: "event13",
-//     },
-//     {
-//         time: "1400",
-//         event: "event14",
-//     },
-//     {
-//         time: "1500",
-//         event: "event15",
-//     },
-//     {
-//         time: "1600",
-//         event: "event16",
-//     },
-//     {
-//         time: "1700",
-//         event: "event17",
-//     }];
-//     for(let i = 0; i < myVar.length; i++)
-//     {
-//         let head = myVar[i].time;
-//         console.log(myVar[i].time);
-//         let newTime = $("<row>");
-//         newTime.attr("id", myVar[i].time);
-//         newTime.addClass("row");
-//         $("#headtable").append(newTime);
-//         let newDivTime = $("<div>");
-//         newDivTime.text(myVar[i].time);
-//         newDivTime.addClass("col-sm-3");
-//         $("#0900").append(newDivTime);
-//         let newDivSched = $("<input>");
-//         newDivSched.text(myVar[i].event);
-//         newDivSched.addClass("col-sm-6 input");
-//         newDivSched.attr("data-name", myVar[i].time);
-//         //console.log(newDivSched);
-//         $("#0900").append(newDivSched);
-//         let newDivBtn = $("<button>");
-//         newDivBtn.text("submit");
-//         newDivBtn.attr("data-name", i);
-//         newDivBtn.addClass("col-sm-3 submitbuttons");
-//         $("#0900").append(newDivBtn);
-//     }
-//     $(".input").on("keyup", function(){
-//         let newstring = this.value;
-
-//         console.log(newstring);
-//         $(".submitbuttons").on("click", function(){
-//             event.preventDefault();
-//             let btnclick = $(this).attr("data-name");
-//             console.log(btnclick);
-//             myVar[btnclick].event = newstring;
-//         })
-//     })
